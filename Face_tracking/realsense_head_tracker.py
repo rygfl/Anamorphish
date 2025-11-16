0aa6f92b9cb5f5132ba54f4a05516a3489d7f146
rygfl <rygfl@naver.com>
2025-11-16 15:10:10 +0900

    Feat: 해상도 변경

--- Face_tracking/realsense_head_tracker.py
+++ Face_tracking/realsense_head_tracker.py
@@ -20,8 +20,8 @@ pipeline = rs.pipeline()
 config = rs.config()
 
 # 해상도/프레임레이트는 필요에 따라 조절
-config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
-config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 60)
+config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 60)
 
 pipeline.start(config)
 
