19273edc904fe8a4ec38223c4741a4bf563c8990
rygfl <rygfl@naver.com>
2025-11-16 13:57:05 +0900

    Test: build 테스트

---
+++ Realsense/Assets/Scripts/Managers/InputManager.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "InputManager", menuName = "Scriptable Objects/InputManager")]
+public class InputManager : ScriptableObject
+{
+    
+}
---
+++ Realsense/Assets/Scripts/Managers/Managers.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class Managers : MonoBehaviour
+{
+    static Managers s_instance;
+    static Managers Instance { get { Init(); return s_instance; } }
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    static void Init()
+    {
+        if (s_instance == null)
+        {
+            GameObject go = GameObject.Find("@Managers");
+            if (go == null)
+            {
+                go = new GameObject { name = "@Managers" };
+                go.AddComponent<Managers>();
+            }
+            DontDestroyOnLoad(go);
+            s_instance = go.GetComponent<Managers>();
+        }
+    }
+}
